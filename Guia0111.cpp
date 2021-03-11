720a13ded0de284120097779b50d6026f83b485e
Luca Salazar <43450396+lucasalazar@users.noreply.github.com>
2021-03-10 23:42:53 -0300

    Create Guia0111.cpp

---
+++ Guia0111.cpp
@@ -0,0 +1,16 @@
+#include "karel.hpp"
+
+void decorateWorld (const char *filename)
+{
+    world->save(filename);
+}
+
+class MyRobot : public Robot
+{
+public:
+void turnLeftN(int turns)
+{
+
+}
+
+}
